6f347a1d52c0dce911372b77af749b092caefc26
Samuel <samuelpaul16012004@gmail.com>
2022-09-09 19:20:28 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,8 +24,8 @@ else if (l == 0)
 {
 	printf("Last digit of %d is %d and is 0\n", n, l);
 }
-else
-{
+else if (l <= 5 && l != 0)
+{ 
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 return (0);
