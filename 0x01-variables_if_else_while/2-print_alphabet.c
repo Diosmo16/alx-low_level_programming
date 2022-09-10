aefab38a811770a35b3b2c68945d3a12df3657bf
Samuel <samuelpaul16012004@gmail.com>
2022-09-09 18:08:28 -0700

    First commit on project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+	int lalpha;
+
+	for(lalpha = 'a'; lalpha <= 'z'; lalpha++)
+	putchar(lalpha);
+	putchar('\n');
+	
+	return lalpha;
+	
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>                                                                                                                                   
+#include <stdlib.h>                                                                                                                                 
+int main ()                                                                                                                                          
+{ 
+
+	        int lalpha;
+		for(lalpha = 'a'; lalpha <= 'z'; lalpha++)
+		putchar(lalpha);
+
+		for(lalpha = 'A'; lalpha <= 'Z'; lalpha++)
+		putchar(lalpha); 
+
+		putchar('\n');
+		return lalpha;
+}
