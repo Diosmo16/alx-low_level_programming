86b3aa5e40584dfeb77d2c50b7d0012eef85a3c7
Samuel <samuelpaul16012004@gmail.com>
2022-09-12 19:06:58 -0700

    The number's game

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  *
  * Return: 1
  */
-int main()
+int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	puts(quote);
 
