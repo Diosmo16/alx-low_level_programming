f943279cff6da3423ac3736ec937531b31d24392
Samuel <samuelpaul16012004@gmail.com>
2022-10-13 12:58:47 -0700

    Done with C hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - prints a line of text to standard output
+ * main - prints a line of text to standard error
  *
  * Return: 1
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	puts(quote);
-
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ int main(void)
 	long long int k;
 	float a;
 
-	printf("Size of a char: %lu\n", (unsigned long)sizeof(e));
-	printf("Size of an int: %lu\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu\n", (unsigned long)sizeof(d));
-	printf("Size of a long long int: %lu\n", (unsigned long)sizeof(k));
-	printf("Size of a float: %lu\n", (unsigned long)sizeof(a));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(a));
 
 return (0);
 }
