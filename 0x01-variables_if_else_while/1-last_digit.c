bff9c5626d5554cd7b07b5d40922021d0444179a
Samuel <samuelpaul16012004@gmail.com>
2022-10-14 13:36:05 -0700

    Corrections made

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
-* main - print if thr number is positive, zero or negative
-* Description: using main function
-* this program prints "Programming is positive, zero or negative
+* main - program to print last digit of a random number
+* Description: this program prints the last digit of a random number
+* then determines if the digit is either greater the 5 OR is 0 OR 
+* is less than 6 and not 0
 * Return: 0
 */
+
 int main(void)
 {
-int n;
-int l;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-l = n % 10;
+	int n;
+	int l;
 
-if (1 > 5)
-{
-	printf("Last digit of %d is %d and is greater then 5\n", n, l);
-}
-else if (l == 0)
-{
-	printf("Last digit of %d is %d and is 0\n", n, l);
-}
-else
-{ 
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-}
-return (0);
-}
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+
+	if (1 > 5)
+	{
+		printf("Last digit of %d is %d and is greater then 5\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else if (l < 6 && l != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	return (0);
+	}
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main ()
+
+/**
+ * main - contains a piece of code that executes a loop
+ * 
+ * Description: the function main contains a piece of code
+ * which prints the alphabet in lowercase using a for loop
+ * Return: 0
+ */
+int main(void)
 {
 	int lalpha;
 
 	for(lalpha = 'a'; lalpha <= 'z'; lalpha++)
 	putchar(lalpha);
 	putchar('\n');
-	
-	return lalpha;
-	
+
+	return (0);
 }
